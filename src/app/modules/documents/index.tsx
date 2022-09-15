bffb8500c7fdbb7d78e2bea6feda6412fe7eb59d
HamzaJoshan <hamzajoshan@gmail.com>
2022-09-15 02:49:50 -0500

    documents page fixed

--- src/app/modules/documents/index.tsx
+++ src/app/modules/documents/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
+import {ShowDataTable}  from './ShowDataTable'
 const Documents = () => {
     const [listData, setListData] = useState([]);
 
@@ -52,7 +52,7 @@ const Documents = () => {
                             checked={dataText}
                             disabled
                         /></td>
-                        : <td style={{ flex: flexValue, textAlign: 'center', padding: '5px 0px' }}>{dataText}</td>
+                        : <td style={{ flex: flexValue, textAlign: 'center', padding: '5px' }}>{dataText}</td>
                 }
             </>
         )
@@ -61,37 +61,7 @@ const Documents = () => {
     return (
         <>
             <p></p>
-            <table style={{ width: '100%', height: 'auto' }}>
-                <thead>
-                    <tr style={{ background: '#28b6e5', padding: '330px 50px' }}>
-                        <HeaderTitleView headerTitle={`DocType`} flexValue={1} />
-                        <HeaderTitleView headerTitle={`Description`} flexValue={3} />
-                        <HeaderTitleView headerTitle={`Link`} flexValue={1} />
-                        <HeaderTitleView headerTitle={`IsApproved`} flexValue={1} />
-                        <HeaderTitleView headerTitle={`IsDeclined`} flexValue={1} />
-                        <HeaderTitleView headerTitle={`Uploaded`} flexValue={1} />
-                        <HeaderTitleView headerTitle={`ActionDate`} flexValue={1} />
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        listData.length && listData.map((item) => {
-                            const { docType, description, hasLink, code, isApproved, isDeclined, uploaded, actionDate } = item
-                            return (
-                                <tr>
-                                    <DataView dataText={docType || ''} flexValue={1} />
-                                    <DataView dataText={description || ''} flexValue={3} />
-                                    <DataView dataText={hasLink ? <Link style={{cursor:'pointer'}} to={`${showDocumentEndpoint}/${code}`} >{code}</Link> : ''} flexValue={1} />
-                                    <DataView dataText={isApproved || ''} flexValue={1} isCheckbox />
-                                    <DataView dataText={isDeclined || ''} flexValue={1} isCheckbox />
-                                    <DataView dataText={uploaded || ''} flexValue={1} />
-                                    <DataView dataText={actionDate || ''} flexValue={1} />
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
+            <ShowDataTable className='mb-5 mb-xl-8 xl-10' listData={listData} showDocumentEndpoint={showDocumentEndpoint} />
 
         </>
     )
